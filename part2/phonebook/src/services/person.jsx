c73de96ee409c5d82cc45d368dc87bee0b9b23f4
fahmirased <fahmi.mjsc@gmail.com>
2023-12-15 00:11:11 +0800

    step10

--- part2/phonebook/src/services/person.jsx
+++ part2/phonebook/src/services/person.jsx
@@ -16,8 +16,13 @@ const remove = (id, name) => {
     return request.then(response => response.data)
 }
 
+const update = (id, updatedPerson) => {
+    const request = axios.put(`${baseUrl}/${id}`, updatedPerson)
+    return request.then(response => response.data)
+}
 export default {
     getAll,
     create,
-    remove
+    remove,
+    update
 }
