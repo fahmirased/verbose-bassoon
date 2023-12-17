ed7096d7164fe10415cf501d778a60ba4b79a3c3
fahmirased <fahmi.mjsc@gmail.com>
2023-12-17 21:00:30 +0800

    step3.3

--- index.js
+++ index.js
@@ -31,6 +31,29 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    console.log(id)
+    const person = persons.find(person => person.id === id)
+    console.log(person)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end('Not Found')
+    }
+    
+})
+
+app.get('/info', (request, response) => {
+    const time = new Date()
+    const numberOfEntries = persons.length
+    
+    const text = `<p>Phonebook has ${numberOfEntries} people<p/><p>${time}</p>` 
+
+    response.send(text)
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
